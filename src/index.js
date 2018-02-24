3fadf65427dec656b7693c0f7f83ff7c14ba9091
JuliaMargun <yuliya.marhun@gmail.com>
2018-02-24 13:58:04 +0300

    A function that calculate how many zeros in the end of factorial of number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var zerosCount = 0;
+  var i = 5;
+  while(i <= number) {
+  	++zerosCount;
+  	var j = i;
+  	do {
+  		j /= 5;
+  		if (j % 5 == 0) {
+  			++zerosCount;
+  		}
+  	} while (j % 5 == 0)
+  	i += 5;
+  }
+  return zerosCount;
 }
