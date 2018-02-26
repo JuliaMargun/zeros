836ee725c5d9a158461f5f4453b28dbe29813721
JuliaMargun <yuliya.marhun@gmail.com>
2018-02-26 12:06:46 +0300

    A new version

--- src/index.js
+++ src/index.js
@@ -2,15 +2,8 @@ module.exports = function getZerosCount(number) {
   var zerosCount = 0;
   var i = 5;
   while(i <= number) {
-  	++zerosCount;
-  	var j = i;
-  	do {
-  		j /= 5;
-  		if (j % 5 == 0) {
-  			++zerosCount;
-  		}
-  	} while (j % 5 == 0)
-  	i += 5;
+  	zerosCount += Math.trunc(number / i);
+  	i *= 5;
   }
   return zerosCount;
 }
